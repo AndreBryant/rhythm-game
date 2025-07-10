7e439ce4dd1e464d401533f56845ffa5b6cbf4e8
embryzon <95128949+embryzon@users.noreply.github.com>
2025-07-11 00:07:37 +0800

    Fix: fix some typo and switched to camelCase

--- src/gameplay_classes/level_parsing.lua
+++ src/gameplay_classes/level_parsing.lua
@@ -1,3 +1,12 @@
+---------------------
+--- Parse Functions
+--- json data for levels
+--- type {beatMap: {metaData: {...meta}, map{...tracks}}}
+---
+--- Improvements:
+--- Use faster sorting algorithm instead of bubble sort
+---------------------
+
 local function bubbleSort(left, right)
 	if left.startMeasure == right.startMeasure then
 		return left.startStep < right.startStep
@@ -7,29 +16,34 @@ local function bubbleSort(left, right)
 end
 
 local function parse(path)
+	-- Import json parsing library
 	local json = require("lib.not-mine.dkjson")
 
+	-- Handle if love camt find file
 	local content, err = love.filesystem.read(path)
 	if not content then
 		return { status = "not-ok", message = "Failed to read file" .. (err or "unknown error"), data = nil }
 	end
 
+	-- Handle if not a proper JSON format
 	local jsonData = json.decode(content)
 	if not jsonData then
 		return { status = "not-ok", message = "Failed to parse Beatmap JSON data.", data = nil }
 	end
 
-	local trackList = jsonData.BeatMap.Map.Track
+	-- Make sure notes are sorted properly in the list
+	local trackList = jsonData.beatMap.map.trackList
 	local data = { tracks = {} }
+
 	for i, track in ipairs(trackList) do
-		if track.Notes then
-			table.sort(track.Notes, bubbleSort)
+		if track.notes then
+			table.sort(track.notes, bubbleSort)
 		end
 	end
+	data.tracks = trackList
 
-	data.tracks = tracklist
-
-	for i, note in ipairs(trackList[1].Notes) do
+	-- Debug
+	for i, note in ipairs(data.tracks[1].notes) do
 		print(
 			"Note Type:\t"
 				.. note.type
